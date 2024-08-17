99f2bb31a33ff75973219e4563a91a8623d24d40
ultrabumbuku <cliche.0308@gmail.com>
2024-08-18 03:29:49 +0900

    Layout.tsxの変更

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Header from './Header'
 import Footer from './Footer'
-import Link from 'next/link'
 
 type LayoutProps = {
   children: React.ReactNode
@@ -16,16 +15,7 @@ const Layout: React.FC<LayoutProps> = ({ children, title = 'ultrabumbuku' }) => 
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Header>
-        <nav>
-          <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/blog">Blog</Link></li>
-            <li><Link href="/kasuga">Kasuga Generator</Link></li>
-            <li><Link href="/random_sushi">Random Sushi</Link></li>
-          </ul>
-        </nav>
-      </Header>
+      <Header />
       <main style={{ flex: 1 }}>{children}</main>
       <Footer />
     </div>
