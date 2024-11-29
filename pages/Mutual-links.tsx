e1cb0dc957b4d32f6680526ed846b6569605f3b1
Yuma Kurokawa <119155669+ultrabumbuku@users.noreply.github.com>
2024-11-29 17:14:19 +0900

    Mutual-linksの追加

---
+++ pages/Mutual-links.tsx
@@ -0,0 +1,18 @@
+import Layout from '../components/Layout';
+
+const MutualLinksPage: React.FC = () => {
+  return (
+    <Layout title="Mutual Links">
+      <section id="links">
+        <h2>Mutual Links</h2>
+        <ul>
+          <li><a href="https://raspi0124.dev" target="_blank">raspi0124</a></li>
+          <li><a href="https://www.eniehack.net/~eniehack/" target="_blank">Eniehack</a></li>
+          <li><a href="https://iorin.io" target="_blank">いおり</a></li>
+        </ul>
+      </section>
+    </Layout>
+  );
+};
+
+export default MutualLinksPage;
