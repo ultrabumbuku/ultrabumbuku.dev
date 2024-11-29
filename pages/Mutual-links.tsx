1e14e7547883dda96eedfe2118d30881ffaaf26b
Yuma Kurokawa <119155669+ultrabumbuku@users.noreply.github.com>
2024-11-29 17:18:28 +0900

    mutual-linksページを追加

--- pages/Mutual-links.tsx
+++ pages/Mutual-links.tsx
@@ -6,9 +6,9 @@ const MutualLinksPage: React.FC = () => {
       <section id="links">
         <h2>Mutual Links</h2>
         <ul>
-          <li><a href="https://raspi0124.dev" target="_blank">raspi0124</a></li>
-          <li><a href="https://www.eniehack.net/~eniehack/" target="_blank">Eniehack</a></li>
-          <li><a href="https://iorin.io" target="_blank">いおり</a></li>
+          <li><a href="https://raspi0124.dev" target="_blank" rel="noopener noreferrer">raspi0124</a></li>
+          <li><a href="https://www.eniehack.net/~eniehack/" target="_blank" rel="noopener noreferrer">Eniehack</a></li>
+          <li><a href="https://iorin.io" target="_blank" rel="noopener noreferrer">いおり</a></li>
         </ul>
       </section>
     </Layout>
