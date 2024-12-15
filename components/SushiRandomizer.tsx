c7b5e68e0ec380ecc05eee827be600a4e8f7a197
Yuma Kurokawa <119155669+ultrabumbuku@users.noreply.github.com>
2024-12-15 21:47:34 +0900

    見た目の変更

--- components/SushiRandomizer.tsx
+++ components/SushiRandomizer.tsx
@@ -678,703 +678,6 @@ const menuItems = [
     {
         "name": "北海道バニラアイス",
         "price": 130
-    },
-
-    {
-        "name": "粉末緑茶18本入り",
-        "price": 120
-    },
-    {
-        "name": "赤だし(カップ)",
-        "price": 200
-    },
-    {
-        "name": "スシロー合わせ醤油",
-        "price": 330
-    },
-    {
-        "name": "スシロー合わせ醤油(甘口)",
-        "price": 330
-    },
-    {
-        "name": "甘だれ230ml",
-        "price": 320
-    },
-    {
-        "name": "厚切り天然インド鮪6貫盛り",
-        "price": 1080
-    },
-    {
-        "name": "大とろ",
-        "price": 120
-    },
-    {
-        "name": "馬刺しねぎとろ包み",
-        "price": 100
-    },
-    {
-        "name": "塩麹漬けまぐろ",
-        "price": 180
-    },
-    {
-        "name": "あん肝軍艦",
-        "price": 180
-    },
-    {
-        "name": "たら白子軍艦",
-        "price": 180
-    },
-    {
-        "name": "スシロー海鮮巻き重ね（冬）",
-        "price": 260
-    },
-    {
-        "name": "厳選まぐろ赤身と中とろの手巻",
-        "price": 360
-    },
-    {
-        "name": "本ずわい蟹の手巻",
-        "price": 360
-    },
-    {
-        "name": "天然ぶり胡麻醤油漬け",
-        "price": 120
-    },
-    {
-        "name": "北海道産にしん酢〆の炙り",
-        "price": 120
-    },
-    {
-        "name": "北海道産サクラマス",
-        "price": 180
-    },
-    {
-        "name": "塩麹漬け赤えび",
-        "price": 180
-    },
-    {
-        "name": "炙り赤えびバター醤油",
-        "price": 180
-    },
-    {
-        "name": "活〆蒸し穴子",
-        "price": 180
-    },
-    {
-        "name": "濃厚うに包み",
-        "price": 180
-    },
-    {
-        "name": "炙り白子と筋子包み",
-        "price": 260
-    },
-    {
-        "name": "本ずわい蟹食べ比べ（ボイル・炙り）",
-        "price": 360
-    },
-    {
-        "name": "神戸牛のにぎり",
-        "price": 360
-    },
-    {
-        "name": "鶏塩らーめん",
-        "price": 460
-    },
-    {
-        "name": "とろ玉明太あんかけうどん",
-        "price": 360
-    },
-    {
-        "name": "かつお香る もずくとおくらの赤だし",
-        "price": 240
-    },
-    {
-        "name": "かつお香る もずくとおくらの味噌汁",
-        "price": 240
-    },
-    {
-        "name": "サーモンのカルパッチョ風サラダパフェ",
-        "price": 250
-    },
-    {
-        "name": "えびのカルパッチョ風サラダパフェ",
-        "price": 250
-    },
-    {
-        "name": "冬の山海の幸 天ぷら盛り（えび天）",
-        "price": 410
-    },
-    {
-        "name": "冬の山海の幸 天ぷら盛り（上えび天）",
-        "price": 460
-    },
-    {
-        "name": "ピスタチオ＆ベリーのケーキ ピスタチオアイス添え",
-        "price": 330
-    },
-    {
-        "name": "懐かしの和梨シャーベット",
-        "price": 130
-    },
-    {
-        "name": "厳選まぐろ赤身",
-        "price": 120
-    },
-    {
-        "name": "一本釣りかつお",
-        "price": 120
-    },
-    {
-        "name": "サーモン",
-        "price": 120
-    },
-    {
-        "name": "オニオンサーモン",
-        "price": 120
-    },
-    {
-        "name": "焼とろサーモン",
-        "price": 120
-    },
-    {
-        "name": "おろし焼とろサーモン",
-        "price": 120
-    },
-    {
-        "name": "サーモンちーず",
-        "price": 120
-    },
-    {
-        "name": "炙りサーモンバジルチーズ",
-        "price": 120
-    },
-    {
-        "name": "えんがわ",
-        "price": 120
-    },
-    {
-        "name": "〆さば",
-        "price": 120
-    },
-    {
-        "name": "〆さば（ごまネギ）",
-        "price": 120
-    },
-    {
-        "name": "赤えび",
-        "price": 120
-    },
-    {
-        "name": "生えび",
-        "price": 120
-    },
-    {
-        "name": "えび",
-        "price": 120
-    },
-    {
-        "name": "えびチーズ",
-        "price": 120
-    },
-    {
-        "name": "えびバジルチーズ",
-        "price": 120
-    },
-    {
-        "name": "えびアボカド",
-        "price": 120
-    },
-    {
-        "name": "甘えび",
-        "price": 120
-    },
-    {
-        "name": "えび天にぎり",
-        "price": 120
-    },
-    {
-        "name": "黒みる貝",
-        "price": 120
-    },
-    {
-        "name": "いか",
-        "price": 120
-    },
-    {
-        "name": "いか塩レモン",
-        "price": 120
-    },
-    {
-        "name": "数の子",
-        "price": 120
-    },
-    {
-        "name": "煮あなご",
-        "price": 120
-    },
-    {
-        "name": "たまご",
-        "price": 120
-    },
-    {
-        "name": "オムライすし",
-        "price": 120
-    },
-    {
-        "name": "豚塩カルビ",
-        "price": 120
-    },
-    {
-        "name": "まるごとハンバーグにぎり",
-        "price": 120
-    },
-    {
-        "name": "生ハム",
-        "price": 120
-    },
-    {
-        "name": "まぐろ・たまご",
-        "price": 120
-    },
-    {
-        "name": "えび・サーモン",
-        "price": 120
-    },
-    {
-        "name": "えび・いか",
-        "price": 120
-    },
-    {
-        "name": "えび・たまご",
-        "price": 120
-    },
-    {
-        "name": "サーモン・焼とろサーモン",
-        "price": 120
-    },
-    {
-        "name": "サーモン・いか",
-        "price": 120
-    },
-    {
-        "name": "サーモン・たまご",
-        "price": 120
-    },
-    {
-        "name": "えび・生えび",
-        "price": 120
-    },
-    {
-        "name": "活〆寒ぶり",
-        "price": 150
-    },
-    {
-        "name": "サーモンのサラダ寿司",
-        "price": 150
-    },
-    {
-        "name": "たいのサラダ寿司",
-        "price": 150
-    },
-    {
-        "name": "たこのサラダ寿司",
-        "price": 150
-    },
-    {
-        "name": "特ネタ中とろ",
-        "price": 180
-    },
-    {
-        "name": "びんとろ",
-        "price": 180
-    },
-    {
-        "name": "生サーモン",
-        "price": 180
-    },
-    {
-        "name": "サーモンアボカド",
-        "price": 180
-    },
-    {
-        "name": "サーモンバジルカマンベール",
-        "price": 180
-    },
-    {
-        "name": "活〆真鯛",
-        "price": 180
-    },
-    {
-        "name": "活〆ぶりとろ",
-        "price": 180
-    },
-    {
-        "name": "天然〆いわし",
-        "price": 180
-    },
-    {
-        "name": "天然〆いわし（ネギ・生姜）",
-        "price": 180
-    },
-    {
-        "name": "大えび",
-        "price": 180
-    },
-    {
-        "name": "上えび天にぎり",
-        "price": 180
-    },
-    {
-        "name": "ボイル本ずわい蟹1貫",
-        "price": 180
-    },
-    {
-        "name": "ほたて貝柱",
-        "price": 180
-    },
-    {
-        "name": "大つぶ貝",
-        "price": 180
-    },
-    {
-        "name": "たこ",
-        "price": 180
-    },
-    {
-        "name": "うなぎの蒲焼き",
-        "price": 180
-    },
-    {
-        "name": "生ハムバジルカマンベール",
-        "price": 180
-    },
-    {
-        "name": "ほたて食べ比べ（焦がし醤油・生レモン・天ぷら）",
-        "price": 260
-    },
-    {
-        "name": "ぷちローセット",
-        "price": 480
-    },
-    {
-        "name": "軍艦ねぎまぐろ",
-        "price": 120
-    },
-    {
-        "name": "まぐろ山かけ",
-        "price": 120
-    },
-    {
-        "name": "まぐろユッケ(卵黄醤油)",
-        "price": 120
-    },
-    {
-        "name": "まぐたく軍艦",
-        "price": 120
-    },
-    {
-        "name": "軍艦甘えび",
-        "price": 120
-    },
-    {
-        "name": "かにみそ",
-        "price": 120
-    },
-    {
-        "name": "たらマヨ",
-        "price": 120
-    },
-    {
-        "name": "数の子松前漬け",
-        "price": 120
-    },
-    {
-        "name": "コーン",
-        "price": 120
-    },
-    {
-        "name": "ツナサラダ",
-        "price": 120
-    },
-    {
-        "name": "シーサラダ",
-        "price": 120
-    },
-    {
-        "name": "カニ風サラダ",
-        "price": 120
-    },
-    {
-        "name": "たまごサラダ",
-        "price": 120
-    },
-    {
-        "name": "うずらフライ軍艦",
-        "price": 120
-    },
-    {
-        "name": "小粒納豆",
-        "price": 120
-    },
-    {
-        "name": "小粒納豆巻(ねぎ抜き)",
-        "price": 120
-    },
-    {
-        "name": "きゅうり巻",
-        "price": 120
-    },
-    {
-        "name": "海老フライアボカドロール",
-        "price": 120
-    },
-    {
-        "name": "いなり",
-        "price": 120
-    },
-    {
-        "name": "鉄火巻",
-        "price": 180
-    },
-    {
-        "name": "まぐたく巻",
-        "price": 180
-    },
-    {
-        "name": "天然筋子包み",
-        "price": 180
-    },
-    {
-        "name": "いくら",
-        "price": 180
-    },
-    {
-        "name": "本ずわい蟹軍艦",
-        "price": 180
-    },
-    {
-        "name": "サーモンいくら軍艦",
-        "price": 260
-    },
-    {
-        "name": "海鮮3貫盛り（甘えび・いくら・本ずわいがに）",
-        "price": 260
-    },
-    {
-        "name": "コク旨まぐろ醤油ラーメン",
-        "price": 390
-    },
-    {
-        "name": "濃厚えび味噌ワンタンメン",
-        "price": 390
-    },
-    {
-        "name": "鯛だし塩ラーメン",
-        "price": 390
-    },
-    {
-        "name": "きつねうどん",
-        "price": 330
-    },
-    {
-        "name": "えび天うどん",
-        "price": 330
-    },
-    {
-        "name": "上えび天うどん",
-        "price": 380
-    },
-    {
-        "name": "かけうどん",
-        "price": 180
-    },
-    {
-        "name": "かつお香る 魚のアラの赤だし",
-        "price": 220
-    },
-    {
-        "name": "かつお香る あおさと海苔の赤だし",
-        "price": 220
-    },
-    {
-        "name": "かつお香る あおさと海苔の味噌汁",
-        "price": 220
-    },
-    {
-        "name": "かつお香る あさりの赤だし",
-        "price": 220
-    },
-    {
-        "name": "かつお香る あさりの味噌汁",
-        "price": 220
-    },
-    {
-        "name": "茶碗蒸し",
-        "price": 230
-    },
-    {
-        "name": "枝豆の茶碗蒸し",
-        "price": 240
-    },
-    {
-        "name": "枝豆のあんかけ茶碗蒸し",
-        "price": 240
-    },
-    {
-        "name": "まるごと海老の柚子こしょう天ぷら",
-        "price": 360
-    },
-    {
-        "name": "赤いかの唐揚げ",
-        "price": 360
-    },
-    {
-        "name": "フライドチキン（2個）",
-        "price": 180
-    },
-    {
-        "name": "フライドチキン（5個）",
-        "price": 360
-    },
-    {
-        "name": "フライドポテト",
-        "price": 150
-    },
-    {
-        "name": "どんぶりポテト",
-        "price": 390
-    },
-    {
-        "name": "かぼちゃの天ぷら",
-        "price": 150
-    },
-    {
-        "name": "生ビール　ジョッキ",
-        "price": 590
-    },
-    {
-        "name": "生ビール　グラス",
-        "price": 430
-    },
-    {
-        "name": "生貯蔵酒",
-        "price": 480
-    },
-    {
-        "name": "翠（SUI）",
-        "price": 400
-    },
-    {
-        "name": "レモンサワー",
-        "price": 400
-    },
-    {
-        "name": "角ハイボール",
-        "price": 400
-    },
-    {
-        "name": "オールフリー（ﾉﾝｱﾙｺｰﾙﾋﾞｰﾙ）",
-        "price": 440
-    },
-    {
-        "name": "ペプシコーラ",
-        "price": 200
-    },
-    {
-        "name": "ポップメロンソーダ",
-        "price": 200
-    },
-    {
-        "name": "さわやか白ぶどう",
-        "price": 200
-    },
-    {
-        "name": "さわやか白ぶどうソーダ",
-        "price": 200
-    },
-    {
-        "name": "ホワイトウォーター",
-        "price": 200
-    },
-    {
-        "name": "ホワイトソーダ",
-        "price": 200
-    },
-    {
-        "name": "ウーロン茶",
-        "price": 200
-    },
-    {
-        "name": "りんごジュース国産100％果汁",
-        "price": 170
-    },
-    {
-        "name": "アイスコーヒー",
-        "price": 170
-    },
-    {
-        "name": "ホットコーヒー",
-        "price": 170
-    },
-    {
-        "name": "アイスカフェラテ",
-        "price": 200
-    },
-    {
-        "name": "ホットカフェラテ",
-        "price": 200
-    },
-    {
-        "name": "アップルジュース",
-        "price": 130
-    },
-    {
-        "name": "オレンジジュース",
-        "price": 130
-    },
-    {
-        "name": "ストロベリーバニラパフェ",
-        "price": 300
-    },
-    {
-        "name": "ショコラケーキリッチ",
-        "price": 230
-    },
-    {
-        "name": "北海道ミルクレープ",
-        "price": 230
-    },
-    {
-        "name": "カタラーナアイスブリュレ",
-        "price": 230
-    },
-    {
-        "name": "わらび餅と大学芋のどっちも盛り",
-        "price": 230
-    },
-    {
-        "name": "大学いも",
-        "price": 130
-    },
-    {
-        "name": "京都峯嵐堂のわらびもち",
-        "price": 150
-    },
-    {
-        "name": "フローズンマンゴー",
-        "price": 150
-    },
-    {
-        "name": "気まぐれダブルアイス／シャーベット",
-        "price": 150
-    },
-    {
-        "name": "懐かしのメロンシャーベット",
-        "price": 130
-    },
-    {
-        "name": "北海道バニラアイス",
-        "price": 130
     }
 ];
 
@@ -1397,8 +700,18 @@ interface Player {
       }
     }, [newPlayerName]);
   
+    const removeSushiRoll = useCallback((playerName: string) => {
+      setCurrentRoll(prev => {
+        const newRoll = { ...prev };
+        delete newRoll[playerName];
+        return newRoll;
+      });
+    }, []);
+  
     const removePlayer = useCallback((index: number) => {
-      setPlayers(prevPlayers => prevPlayers.filter((_, i) => i !== index));
+      if (window.confirm('このプレイヤーを削除してもよろしいですか？')) {
+        setPlayers(prevPlayers => prevPlayers.filter((_, i) => i !== index));
+      }
     }, []);
   
     const rollSushi = useCallback(() => {
@@ -1450,42 +763,62 @@ interface Player {
           />
           <button onClick={addPlayer}>プレイヤーを追加</button>
         </div>
-        <button onClick={rollSushi}>回す</button>
-        <button onClick={resetGame}>リセット</button>
-        <button onClick={handleCheckout}>お会計</button>
+        <div className={styles.actionButtons}>
+          <button onClick={rollSushi}>回す</button>
+          <button onClick={resetGame}>リセット</button>
+          <button onClick={handleCheckout}>お会計</button>
+        </div>
         
         {players.map((player, index) => (
-          <PlayerComponent 
-            key={player.name}
-            player={player}
-            currentRoll={currentRoll[player.name]}
-            onRemove={() => removePlayer(index)}
-            onSelect={() => selectSushi(player.name)}
-          />
+          <div key={player.name} className={styles.playerCard}>
+            <div className={styles.playerHeader}>
+              <span className={styles.playerName}>{player.name}</span>
+              <button 
+                className={styles.removePlayerButton}
+                onClick={() => removePlayer(index)}
+                title="プレイヤーを削除"
+              >
+                ✕
+              </button>
+            </div>
+  
+            {currentRoll[player.name] && (
+              <div className={styles.currentRoll}>
+                <span>{currentRoll[player.name].name} ({currentRoll[player.name].price}円)</span>
+                <div className={styles.rollActions}>
+                  <button 
+                    className={styles.selectButton}
+                    onClick={() => selectSushi(player.name)}
+                  >
+                    選択
+                  </button>
+                  <button 
+                    className={styles.removeRollButton}
+                    onClick={() => removeSushiRoll(player.name)}
+                  >
+                    ✕
+                  </button>
+                </div>
+              </div>
+            )}
+  
+            <div className={styles.orderHistory}>
+              <h4>注文履歴:</h4>
+              {player.orders.map((order, idx) => (
+                <div key={idx} className={styles.orderItem}>
+                  <span>{order.name}</span>
+                  <span>{order.price}円</span>
+                </div>
+              ))}
+              <div className={styles.totalAmount}>
+                合計: {player.totalAmount}円
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     );
   };
   
-  interface PlayerComponentProps {
-    player: Player;
-    currentRoll?: { name: string; price: number };
-    onRemove: () => void;
-    onSelect: () => void;
-  }
-  
-  const PlayerComponent: React.FC<PlayerComponentProps> = ({ player, currentRoll, onRemove, onSelect }) => (
-    <div className={styles.player}>
-      <span>{player.name}</span>
-      <button onClick={onRemove}>削除</button>
-      {currentRoll && (
-        <>
-          <span>{currentRoll.name} ({currentRoll.price}円)</span>
-          <button onClick={onSelect}>選択</button>
-        </>
-      )}
-      <div>合計: {player.totalAmount}円</div>
-    </div>
-  );
-  
   export default SushiRandomizer;
+  
